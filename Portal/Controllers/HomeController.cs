af7e242584e4b07273503a46b6c8f5d50160b500
ahmerali1813 <ahmerali1813@outlook.com>
2025-09-16 20:21:29 +0500

    Add in Home Controller

--- Portal/Controllers/HomeController.cs
+++ Portal/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Portal.Data;
 using Portal.Models;
 
 namespace Portal.Controllers
@@ -7,16 +10,117 @@ namespace Portal.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly PortalSQL sql;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DbContextOptions<PortalContext> dbContextOptions)
         {
             _logger = logger;
+            sql = new PortalSQL(dbContextOptions);
         }
+
+
+
 
         public IActionResult Index()
         {
-            return View();
+            return View(sql.GetAllWheelSections());
         }
+
+        [HttpGet]
+        public IActionResult GetWheelSections()
+        {
+            try
+            {
+
+                var sections = sql.GetAllWheelSections();
+
+
+
+                var dropDownData = sql.GetAllDropDownOfSegment();
+
+                // ✅ Serialize with Newtonsoft.Json inside controller
+                var json = JsonConvert.SerializeObject(
+                    new { success = true, data = dropDownData, wheeldata = sections },
+                    new JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore, // 👈 breaks the cycle
+                        NullValueHandling = NullValueHandling.Ignore,
+                        Formatting = Formatting.Indented,
+                        //ContractResolver = new CasePropertyNamesContractResolver() // 👈 lowercase
+                    }
+                );
+
+                // Return as raw JSON (not as string)
+                return Content(json, "application/json");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        public bool AddWheelSection(string Name, string Colour, int orderID, int fkParentID)
+        {
+            try
+            {
+                WheelSection ws = new WheelSection();
+                ws.Name = Name;
+                ws.Colour = Colour;
+                ws.Order = orderID;
+                if (fkParentID != 0)
+                {
+                    ws.FkParentWheelId = fkParentID;
+                }
+                sql.AddWheelSection(ws);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public WheelSection? EditWheelSection(int id)
+        {
+            return sql.GetWheelSection(id);
+        }
+
+        public bool EditWheelSectionFinal(int id, string Name, string Colour, int orderID, int fkParentID)
+        {
+            try
+            {
+                WheelSection ws = sql.GetWheelSection(id);
+                ws.Name = Name;
+                ws.Colour = Colour;
+                ws.Order = orderID;
+                ws.FkParentWheelId = fkParentID;
+                sql.Save();
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        public bool DeleteWheelSection(int id)
+        {
+            try
+            {
+                sql.DeleteWheelSection(sql.GetWheelSection(id));
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
 
         public IActionResult Privacy()
         {
